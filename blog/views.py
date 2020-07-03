3bac09b1d1117375bf7ae029eda038a0e2be829a
ayo <hakeemanibaba@yahoo.com>
2020-07-03 11:03:04 +0100

    I added user authentication

--- blog/views.py
+++ blog/views.py
@@ -4,19 +4,24 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import login, authenticate, user_logged_in
+from django.core.exceptions import PermissionDenied 
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render (request, 'blog/post_list.html', {'posts':posts})
+    
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 
+
+@permission_required('blog.Post')
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -30,6 +35,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@permission_required('blog.Post')
 def post_edit(request,pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == 'POST':
@@ -44,7 +50,24 @@ def post_edit(request,pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@permission_required('blog.Post')
 def post_delete(request,pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/signup.html', {'form': form})
+
+
